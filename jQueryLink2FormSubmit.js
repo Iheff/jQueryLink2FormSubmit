b14f545ebe82d2936472aeca944bcd9c07dc0592
Steffen Cope <steffencope@googlemail.com>
2011-09-23 23:54:18 +0100

    add some code

--- jQueryLink2FormSubmit.js
+++ jQueryLink2FormSubmit.js
@@ -1,6 +1,28 @@
-$document.ready( function(){
-	
-$('form:first;').submit();	
+function getUrlVars()
+{
+    var vars = [], hash;
+    var hashes = window.location.href.slice(window.location.href.indexOf('?') + 1).split('&');
+    for(var i = 0; i < hashes.length; i++)
+    {
+        hash = hashes[i].split('=');
+        vars.push(hash[0]);
+        vars[hash[0]] = hash[1];
+    }
+    return vars;
+}
+
+$(document).ready( function(){
+	var inputID = '#ContentPlaceHolder1_BasketItems1_txtVoucher';
+	 if(window.location.contains("voucher=")) // This doesn't work, any suggestions.
+                		{
+							//alert("This is a voucher Url");
+							var voucherCode = getUrlVars()["voucher"];
+							
+							$('form:first input #'+ inputID ).attr('val',voucherCode);
+							
+							$('form:first').submit();	 
+						}
+
 	
 	});
 
